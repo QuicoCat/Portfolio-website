cb02c2213ce7ab4745e9fb3a73103446e0e0156a
r.maganinho <113379890+QuicoCat@users.noreply.github.com>
2023-08-03 19:54:09 +0100

    Add second, third and fourth projects.

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,7 @@
 let firstButton = document.getElementById("first-button");
+let secondButton = document.getElementById("second-button");
+let thirdButton = document.getElementById("third-button");
+let fourthButton = document.getElementById("fourth-button");
 
 const buttonOpener = (button, link) => {
     button.addEventListener('click', function() {
@@ -7,5 +10,6 @@ const buttonOpener = (button, link) => {
 }
 
 buttonOpener(firstButton, './fictional-club/fictional-club.html');
-
-
+buttonOpener(secondButton, 'https://github.com/QuicoCat/message_generator');
+buttonOpener(thirdButton, 'https://github.com/QuicoCat/JavascriptCodecademy/blob/main/crediccardchecker.js');
+buttonOpener(fourthButton, 'https://github.com/QuicoCat/JavascriptCodecademy/blob/main/mysteriousorganismchallenge.js');
